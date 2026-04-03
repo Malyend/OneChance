ab1303be448bc4da4c4f37eb4cb7bcfabb6aeb72
malyend <191012129+Malyend@users.noreply.github.com.>
2026-04-03 19:10:56 +0000

    Service Worker is working
    Edited the Css slightly,
    the Font is now similar to the Iphone.
    
    Added Active psuedoclass to Confirm(Submit) and
    Exit Buttons.
    
    Added a favicon.
    
    Edited the JSON OneChance file, more than anything
    Unsure if it works but i'll check later on.
    
    HTML needs to be checked too.

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,13 @@
-self.addEventListener('install', () => {
-    console.log('service worker Installed!')
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('OneChance-v1').then((cache) => {
+        return cache.addAll([
+            './',
+            './index.html',
+            './toDo.Css',
+            './toDo.js',
+            './serviceWorker.js'
+            ])
+        })
+    )
 })
